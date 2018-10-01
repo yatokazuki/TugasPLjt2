5cc1823782d4a86c3e6a6ba48d8bfd892b6765b3
yatokazuki <43543171+yatokazuki@users.noreply.github.com>
2018-10-01 23:48:38 +0700

    Add files via upload

---
+++ Tugas2Pljt/WindowsFormsConvert/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibraryConvert;
+namespace WindowsFormsConvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        Terbilang convert = new Terbilang();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var hasil = convert.TerbilangIndonesia(int.Parse(textBox1.Text));
+            textBox2.Text = hasil.ToString();
+        }
+    }
+}
